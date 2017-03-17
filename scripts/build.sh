0e9f5812c7e1c241ff95b46b15340511e033d4a0
annatisch <antisch@microsoft.com>
2017-03-17 14:11:31 -0700

    skip 3.5

--- scripts/build.sh
+++ scripts/build.sh
@@ -14,7 +14,7 @@ python -m azure.cli -h
 
 # PyLint does not yet support Python 3.6 https://github.com/PyCQA/pylint/issues/1241
 LOCAL_PYTHON_VERSION=$(python -c 'import sys; print("{0}.{1}".format(sys.version_info[0], sys.version_info[1]))')
-if [[ "$TRAVIS_PYTHON_VERSION" == "3.6" || "$LOCAL_PYTHON_VERSION" == "3.6" ]]; then
+if [[ "$TRAVIS_PYTHON_VERSION" != "2.7" || "$LOCAL_PYTHON_VERSION" != "2.7" ]]; then
     echo 'Skipping check_style since it is not supported in python 3.6'
 else
     check_style --ci;
