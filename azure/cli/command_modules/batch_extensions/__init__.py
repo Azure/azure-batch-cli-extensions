affd79ad87d6ead3a93dc80ee539b716c7e4ba93
annatisch <antisch@microsoft.com>
2017-03-17 13:36:29 -0700

    removed print

--- azure/cli/command_modules/batch_extensions/__init__.py
+++ azure/cli/command_modules/batch_extensions/__init__.py
@@ -4,11 +4,14 @@
 # --------------------------------------------------------------------------------------------
 
 import azure.cli.command_modules.batch_extensions._help  # pylint: disable=unused-import
+from .version import VERSION
 
 
 def load_params(_):
-    import azure.cli.command_modules.batch_extensions._params  # pylint: disable=redefined-outer-name
+    import azure.cli.command_modules.batch_extensions._params  # pylint: disable=unused-variable,redefined-outer-name
 
 
 def load_commands():
-    import azure.cli.command_modules.batch_extensions.commands  # pylint: disable=redefined-outer-name
+    import azure.cli.command_modules.batch_extensions.commands  # pylint: disable=unused-variable,redefined-outer-name
+
+__version__ = VERSION
