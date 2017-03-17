97866c3d5e78b953c379593f003c949cf0cf8ccb
xingwu1 <xingwu1@yahoo.com>
2017-03-17 13:00:58 -0700

    Get rid of pylint warning.

--- src/command_modules/azure-cli-batch-extensions/azure/cli/command_modules/batch_extensions/__init__.py
+++ src/command_modules/azure-cli-batch-extensions/azure/cli/command_modules/batch_extensions/__init__.py
@@ -7,8 +7,8 @@ import azure.cli.command_modules.batch_extensions._help  # pylint: disable=unuse
 
 
 def load_params(_):
-    import azure.cli.command_modules.batch_extensions._params  # pylint: disable=redefined-outer-name
+    import azure.cli.command_modules.batch_extensions._params  # pylint: disable=unused-variable,redefined-outer-name
 
 
 def load_commands():
-    import azure.cli.command_modules.batch_extensions.commands  # pylint: disable=redefined-outer-name
+    import azure.cli.command_modules.batch_extensions.commands  # pylint: disable=unused-variable,redefined-outer-name
