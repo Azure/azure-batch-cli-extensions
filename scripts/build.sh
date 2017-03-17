007b4c0b0dd54f90d29699c06b4ed0fb9c9c621a
annatisch <antisch@microsoft.com>
2017-03-17 13:47:57 -0700

    fixes

--- scripts/build.sh
+++ scripts/build.sh
@@ -13,7 +13,6 @@ export PYTHONPATH=$PATHONPATH:./src
 python -m azure.cli -h
 
 # PyLint does not yet support Python 3.6 https://github.com/PyCQA/pylint/issues/1241
-check_style --ci;
 LOCAL_PYTHON_VERSION=$(python -c 'import sys; print("{0}.{1}".format(sys.version_info[0], sys.version_info[1]))')
 if [[ "$TRAVIS_PYTHON_VERSION" == "3.6" || "$LOCAL_PYTHON_VERSION" == "3.6" ]]; then
     echo 'Skipping check_style since it is not supported in python 3.6'
