98a47dda60d69046f9b09d387f88b62d68c6add6
Brandon Klein <bgklein@wisc.edu>
2019-02-07 09:50:48 -0800

    Add blob source back as a map to http_url and update comments

--- azext/batch/models/extended_resource_file_py3.py
+++ azext/batch/models/extended_resource_file_py3.py
@@ -9,13 +9,34 @@ from azure.batch.models import ResourceFile
 class ExtendedResourceFile(ResourceFile):
     """A file to be downloaded from Azure blob storage to a compute node.
 
-    :param batch_url: The URL of the file within Azure Blob Storage. This
+    :param http_url: The URL of the file within Azure Blob Storage. This
      URL must be readable using anonymous access; that is, the Batch service
      does not present any credentials when downloading the blob. There are two
      ways to get such a URL for a blob in Azure storage: include a Shared
      Access Signature (SAS) granting read permissions on the blob, or set the
      ACL for the blob or its container to allow public access.
-    :type batch_url: str
+    :type http_url: str
+    :param auto_storage_container_name: The storage container name in the auto
+    storage account. The autoStorageContainerName, storageContainerUrl and
+    httpUrl properties are mutually exclusive and one of them must be specified.
+    :type auto_storage_container_name: str
+    :param storage_container_url: The URL of the blob container within Azure
+    Blob Storage. The autoStorageContainerName, storageContainerUrl and httpUrl
+    properties are mutually exclusive and one of them must be specified. This
+    URL must be readable and listable using anonymous access; that is, the
+    Batch service does not present any credentials when downloading blobs from
+    the container. There are two ways to get such a URL for a container in
+    Azure storage: include a Shared Access Signature (SAS) granting read and
+    list permissions on the container, or set the ACL for the container to
+    allow public access.
+    :type storage_container_url: str
+    :param blob_prefix: The blob prefix to use when downloading blobs from an
+    Azure Storage container. Only the blobs whose names begin with the specified
+    prefix will be downloaded. The property is valid only when
+    autoStorageContainerName or storageContainerUrl is used. This prefix can be
+    a partial filename or a subdirectory. If a prefix is not specified, all the
+    files in the container will be downloaded.
+    :type blob_prefix: str
     :param file_path: The location on the compute node to which to download
      the file, relative to the task's working directory. If using a file group
      source that references more than one file, this will be considered the name
@@ -37,15 +58,28 @@ class ExtendedResourceFile(ResourceFile):
 
     _attribute_map = {
         'http_url': {'key': 'httpUrl', 'type': 'str'},
+        'auto_storage_container_name': {'key': 'autoStorageContainerName', 'type': 'str'},
+        'blob_prefix': {'key': 'blobPrefix', 'type': 'str'},
+        'storage_container_url': {'key': 'storageContainerUrl', 'type': 'str'},
         'file_path': {'key': 'filePath', 'type': 'str'},
         'file_mode': {'key': 'fileMode', 'type': 'str'},
         'source': {'key': 'source', 'type': 'FileSource'}
     }
 
-    def __init__(self, *, http_url: str=None, file_path: str=None,
-                 file_mode: str=None, source=None, **kwargs) -> None:
+    def __init__(self,
+                 *,
+                 http_url: str=None,
+                 auto_storage_container_name: str=None,
+                 storage_container_url: str=None,
+                 blob_prefix: str=None,
+                 file_path: str=None,
+                 file_mode: str=None,
+                 source=None, **kwargs) -> None:
         super(ExtendedResourceFile, self).__init__(
             http_url=http_url,
+            auto_storage_container_name=auto_storage_container_name,
+            storage_container_url=storage_container_url,
+            blob_prefix=blob_prefix,
             file_path=file_path,
             file_mode=file_mode,
             **kwargs)
