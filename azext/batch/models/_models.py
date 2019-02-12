95f69200c7508897344e1590760f66a1c2e12012
Brandon Klein <bgklein@wisc.edu>
2019-02-12 14:42:26 -0800

    add license headers

--- azext/batch/models/_models.py
+++ azext/batch/models/_models.py
@@ -1,5 +1,9 @@
-# pylint: disable=too-many-lines
+# --------------------------------------------------------------------------------------------
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# Licensed under the MIT License. See License.txt in the project root for license information.
+# --------------------------------------------------------------------------------------------
 
+# pylint: disable=too-many-lines
 
 import os
 import azure.batch.models as models
